64567702f42be45ce467395c3cbe7c668acb59b9
Hassan Majaro <hhassanzino75@gmail.com>
2025-08-22 07:01:51 -0700

    remove

--- week3/majaro_hassan_task9/6contact_lookup.py
+++
@@ -1,9 +0,0 @@
-''' Task5: Contact Quick Lookup
-    Store three names and their phone numbers in two separate tuples
-        Create a dictionary from the using dict(zip(....)).
-        Ask the user for a name and display the corresponding number (or an error message).
-    Requirements:
-        use zip() and dict() to combine tuples
-        use .get() for safe retrieval
-        no loops.
-'''
